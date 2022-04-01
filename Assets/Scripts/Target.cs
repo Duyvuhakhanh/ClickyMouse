47b8b2f1541cdd16a7802d05c89d9fdf341e7a26
Duy <duyvhk.ilec@gmail.com>
2022-04-01 14:36:59 +0700

    + add clicky mosue project

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float maxSpeed = 16;
+    private float minSpeed = 14;
+    private float xSpawnPos = 4;
+    private float ySpawnPos = -2;
+    private float maxTorque = 10;
+    public int pointValue;
+    public ParticleSystem explosionPartical;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionPartical, transform.position, explosionPartical.transform.rotation);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xSpawnPos, xSpawnPos), ySpawnPos);
+    }
+}
